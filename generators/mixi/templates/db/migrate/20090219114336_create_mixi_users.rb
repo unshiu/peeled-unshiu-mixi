e7876e75fd6d6c9437614c3aa8a97ee53a3ae4de
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2009-03-20 09:54:32 +0000

    コピー忘れ
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/mixi/trunk@69156 6b3104e4-9f14-0410-8847-ebdc78b6de3d

---
+++ generators/mixi/templates/db/migrate/20090219114336_create_mixi_users.rb
@@ -0,0 +1,19 @@
+class CreateMixiUsers < ActiveRecord::Migration
+  def self.up
+    create_table :mixi_users do |t|
+      t.string   :mixi_id,      :null => false
+      t.integer  :base_user_id, :null => false
+      t.string   :nickname
+      t.string   :profile_url
+      t.string   :thumbnail_url
+      t.datetime :deleted_at
+      
+      t.timestamps
+    end
+    add_index :mixi_users, :mixi_id, :unique => true
+  end
+
+  def self.down
+    drop_table :mixi_users
+  end
+end
