31b4dd0486789aa9c96fd6c9c5a093a6f4ac6921
gaooh <silver.vine.ebisen@gmail.com>
2009-07-28 17:39:06 +0900

    きちんとmodule化

--- generators/mixi/templates/app/helpers/manage/mixi_inflow_masters_helper.rb
+++ generators/mixi/templates/app/helpers/manage/mixi_inflow_masters_helper.rb
@@ -1,13 +1,3 @@
 module Manage::MixiInflowMastersHelper
-  
-  def editable_master
-    if @mixi_inflow_master.id == MixiInflowMaster::TOTAL || @mixi_inflow_master.id == MixiInflowMaster::OTHER
-      false
-    else
-      true
-    end
-  end
-  
-  alias deletable_master editable_master
-  alias publish_master editable_master
+  include ManageMixiInflowMastersModule
 end
