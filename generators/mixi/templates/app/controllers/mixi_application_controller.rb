fb22a4f6b9efb0b601e5f8d9babf2b8ab085579f
gaooh <silver.vine.ebisen@gmail.com>
2009-07-28 17:36:10 +0900

    不必要なのがマージされていた

--- generators/mixi/templates/app/controllers/mixi_application_controller.rb
+++ generators/mixi/templates/app/controllers/mixi_application_controller.rb
@@ -1,11 +1,3 @@
 class MixiApplicationController < ApplicationController
   include MixiApplicationControllerModule
-  before_filter :color_filter
-  
-private
-
-  def color_filter
-    @baton_color = params[:baton_color].nil? ? BatonColor::RED : params[:baton_color].to_i
-  end
-  
 end
--- generators/mixi/templates/app/controllers/mixi_gadget_controller.rb
+++ generators/mixi/templates/app/controllers/mixi_gadget_controller.rb
@@ -2,11 +2,6 @@ class MixiGadgetController < MixiApplicationController
   include MixiGadgetControllerModule
   
   def top
-    @mixi_user = current_mixiapp_viewer
-    @batons = Baton.by_color(@baton_color).recent(8)
-    @activities = MixiActivity.find(:all, :conditions => ['receipt_mixi_user_id = ?', @mixi_user.id], 
-                                    :limit => 10, :order => 'created_at desc')
-    @friends = @mixi_user.recent_baton_answer_friend_with_paginate(4, 0)
   end
 
 end
