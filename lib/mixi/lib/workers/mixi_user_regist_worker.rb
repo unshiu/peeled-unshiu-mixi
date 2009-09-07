9042efce9a90259b9e5ba5abd21ff35244293972
gaooh <silver.vine.ebisen@gmail.com>
2009-09-07 20:13:07 +0900

    backgroundrbでの遅延処理ができてなかった

--- lib/mixi/lib/workers/mixi_user_regist_worker.rb
+++ lib/mixi/lib/workers/mixi_user_regist_worker.rb
@@ -3,6 +3,7 @@
 # リアルタイムで処理する必要がない以下のような処理をバックグラウンドで行う
 #  友人関係の逆のリレーション
 #  一度登録された友人関係がなくなっている場合の対応
+#   削除する際はデータの性質上、レコード数が増えることが懸念されかつ、過去データの保持をアプリ側でする意味がないので物理削除する
 #
 module MixiUserRegistWorkerModule
   
@@ -16,12 +17,13 @@ module MixiUserRegistWorkerModule
   
   def create(args = nil)
     # this method is called, when worker is loaded for the first time
+    MixiFriend.new # 宣言しないとbackgroundがMixiFriendをloadできなといわれおちてしまう
   end
   
   def mixi_user_regist(request)
     mixi_user = request[:mixi_user]
     mixi_user_friends = request[:mixi_friends]
-     
+    
     mixi_user_friends.each do | mixi_user_friend |
       mixi_user_friend.mixi_friends << mixi_user unless mixi_user_friend.mixi_friends.member?(mixi_user)
     end
@@ -29,10 +31,10 @@ module MixiUserRegistWorkerModule
     friend_ids = mixi_user_friends.collect { |mixi_user_friend| mixi_user_friend.id }
     mixi_user = MixiUser.find(mixi_user.id)
     mixi_friend_ships = mixi_user.mixi_friend_ships
-    mixi_friend_ships.delete_if { |mixi_friend_ship| friend_ids.member?(mixi_friend_ship.friend_id) }
+    mixi_friend_ships = mixi_friend_ships.delete_if { |mixi_friend_ship| friend_ids.member?(mixi_friend_ship.friend_id) }
     
     mixi_friend_ships.each do |mixi_friend_ship|
-      mixi_friend_ship.destroy
+      mixi_friend_ship.destroy! 
     end
   end
   
