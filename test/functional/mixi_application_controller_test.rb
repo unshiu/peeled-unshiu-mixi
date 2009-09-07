adb385a3e9a760dba3043679f8690db49995333f
gaooh <silver.vine.ebisen@gmail.com>
2009-09-07 20:10:14 +0900

    テストもあわせて

--- test/functional/mixi_application_controller_test.rb
+++ test/functional/mixi_application_controller_test.rb
@@ -33,7 +33,7 @@ module MixiApplicationControllerTestModule
   
   define_method('test: current_mixiapp_viewer は現在アプリを閲覧しているユーザ情報を取得する') do
     
-    post :check_mixiapp_viewer, :viewer => 1
+    post :check_mixiapp_viewer, :opensocial_viewer_id => 1
     assert_response :success
     
     assert_equal(assigns["viewer"], MixiUser.find_by_mixi_id(1))
@@ -41,8 +41,8 @@ module MixiApplicationControllerTestModule
   
   define_method('test: current_mixiapp_viewer はsessionにユーザ情報を持っていたらsession側を正とし、パラメータでわたってきても無視する') do
     
-    session[:viewer] = MixiUser.find_by_mixi_id(2)
-    post :check_mixiapp_viewer, :viewer => 1
+    session[:opensocial_viewer] = MixiUser.find_by_mixi_id(2)
+    post :check_mixiapp_viewer, :opensocial_viewer_id => 1
     assert_response :success
     
     assert_equal(assigns["viewer"], MixiUser.find_by_mixi_id(2))
@@ -50,7 +50,7 @@ module MixiApplicationControllerTestModule
   
   define_method('test: current_mixiapp_owner は現在アプリの所有者ユーザ情報を取得する') do
     
-    post :check_mixiapp_owner, :owner => 1
+    post :check_mixiapp_owner, :opensocial_owner_id => 1
     assert_response :success
     
     assert_equal(assigns["owner"], MixiUser.find_by_mixi_id(1))
@@ -58,18 +58,11 @@ module MixiApplicationControllerTestModule
   
   define_method('test: current_mixiapp_owner はsessionにユーザ情報を持っていたらsession側を正とし、パラメータでわたってきても無視する') do
     
-    session[:owner] = MixiUser.find_by_mixi_id(2)
-    post :check_mixiapp_owner, :viewer => 1
+    session[:opensocial_owner] = MixiUser.find_by_mixi_id(2)
+    post :check_mixiapp_owner, :opensocial_owner_id => 1
     assert_response :success
     
     assert_equal(assigns["owner"], MixiUser.find_by_mixi_id(2))
   end
   
-  define_method('test: current_mixiapp_owner は opensocial_owner_id でもパラメータを処理できる') do
-    
-    post :check_mixiapp_owner, :opensocial_owner_id => 1
-    assert_response :success
-    
-    assert_equal(assigns["owner"], MixiUser.find_by_mixi_id(1))
-  end
 end
