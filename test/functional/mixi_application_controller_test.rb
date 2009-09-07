b197539d5434ce7da982637f0bf4247d12e792a4
gaooh <silver.vine.ebisen@gmail.com>
2009-09-07 20:07:44 +0900

    parameterを変更

--- test/functional/mixi_application_controller_test.rb
+++ test/functional/mixi_application_controller_test.rb
@@ -2,6 +2,16 @@
 class MixiApplicationController
   include MixiApplicationControllerModule
   
+  def check_mixiapp_viewer
+    @viewer = current_mixiapp_viewer
+    render :text => "OK"
+  end
+  
+  def check_mixiapp_owner
+    @owner = current_mixiapp_owner
+    render :text => "OK"
+  end
+  
   def signature_require
     return true
   end
@@ -21,10 +31,45 @@ module MixiApplicationControllerTestModule
     end
   end
   
-  define_method('test: remote_token は認証用のtokenを発行する') do
-      
-    post :remote_token
+  define_method('test: current_mixiapp_viewer は現在アプリを閲覧しているユーザ情報を取得する') do
+    
+    post :check_mixiapp_viewer, :viewer => 1
     assert_response :success
     
+    assert_equal(assigns["viewer"], MixiUser.find_by_mixi_id(1))
+  end
+  
+  define_method('test: current_mixiapp_viewer はsessionにユーザ情報を持っていたらsession側を正とし、パラメータでわたってきても無視する') do
+    
+    session[:viewer] = MixiUser.find_by_mixi_id(2)
+    post :check_mixiapp_viewer, :viewer => 1
+    assert_response :success
+    
+    assert_equal(assigns["viewer"], MixiUser.find_by_mixi_id(2))
+  end
+  
+  define_method('test: current_mixiapp_owner は現在アプリの所有者ユーザ情報を取得する') do
+    
+    post :check_mixiapp_owner, :owner => 1
+    assert_response :success
+    
+    assert_equal(assigns["owner"], MixiUser.find_by_mixi_id(1))
+  end
+  
+  define_method('test: current_mixiapp_owner はsessionにユーザ情報を持っていたらsession側を正とし、パラメータでわたってきても無視する') do
+    
+    session[:owner] = MixiUser.find_by_mixi_id(2)
+    post :check_mixiapp_owner, :viewer => 1
+    assert_response :success
+    
+    assert_equal(assigns["owner"], MixiUser.find_by_mixi_id(2))
+  end
+  
+  define_method('test: current_mixiapp_owner は opensocial_owner_id でもパラメータを処理できる') do
+    
+    post :check_mixiapp_owner, :opensocial_owner_id => 1
+    assert_response :success
+    
+    assert_equal(assigns["owner"], MixiUser.find_by_mixi_id(1))
   end
 end
